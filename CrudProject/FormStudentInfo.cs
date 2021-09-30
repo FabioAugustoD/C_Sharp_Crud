bbf8adf333287a8d2d85c75d7831baaf6262dfe1
FabioAugustoD <fabioaugustodametto@gmail.com>
2021-09-29 23:11:31 -0300

    View form
    Create FormStudentInfo

---
+++ CrudProject/FormStudentInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CrudProject
+{
+    public partial class FormStudentInfo : Form
+    {
+        public FormStudentInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
